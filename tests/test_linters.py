d895d952ac8287e87714058d0f9b2a8b8bf61cde
Pawel F <pawe.93945@gmail.com>
2020-02-19 22:37:00 +0100

    test for black, flake8 E501 delete, #7

--- tests/test_linters.py
+++ tests/test_linters.py
@@ -2,6 +2,8 @@ from __future__ import print_function
 
 from subprocess import CalledProcessError, check_output
 
+BLACK_COMMAND = "black"
+BLACK_OPTIONS = ["--check"]
 FLAKE8_COMMAND = "flake8"
 PYLINT_COMMAND = "pylint"
 LINTER_INPUTS = ["aidriv", "tests"]
@@ -19,9 +21,16 @@ def test_pylint(directory):
     _test_linter(PYLINT_COMMAND, directory)
 
 
-def _test_linter(command, path):
+def test_black(directory):
+    _test_linter(BLACK_COMMAND, directory, BLACK_OPTIONS)
+
+
+def _test_linter(command, path, options=None):
+    cmd = [command, path]
+    if options is not None:
+        cmd += options
     try:
-        check_output([command, path])
+        check_output(cmd)
     except CalledProcessError as e:
         raise AssertionError(
             f"{command} has found errors.\n\n {e.output.decode('utf8')}"
