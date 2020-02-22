e7a9d0d27859aac7908aab23c2a0715fc8b9aa85
Pawel F <pawe.93945@gmail.com>
2020-02-22 15:19:30 +0100

    small change in tests, #7

--- tests/test_linters.py
+++ tests/test_linters.py
@@ -3,7 +3,7 @@ from __future__ import print_function
 from subprocess import CalledProcessError, check_output
 
 BLACK_COMMAND = "black"
-BLACK_OPTIONS = ["--check"]
+BLACK_OPTION = "--check"
 FLAKE8_COMMAND = "flake8"
 PYLINT_COMMAND = "pylint"
 LINTER_INPUTS = ["aidriv", "tests"]
@@ -22,15 +22,12 @@ def test_pylint(directory):
 
 
 def test_black(directory):
-    _test_linter(BLACK_COMMAND, directory, BLACK_OPTIONS)
+    _test_linter(BLACK_COMMAND, directory, BLACK_OPTION)
 
 
-def _test_linter(command, path, options=None):
-    cmd = [command, path]
-    if options is not None:
-        cmd += options
+def _test_linter(command, path, option=None):
     try:
-        check_output(cmd)
+        check_output([command, path, option or ""])
     except CalledProcessError as e:
         raise AssertionError(
             f"{command} has found errors.\n\n {e.output.decode('utf8')}"
